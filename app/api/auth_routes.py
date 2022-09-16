11f1f9bd778173f94088fa10f769f572929267e0
Brad Simpson <bradsimpson@icloud.com>
2022-09-16 12:45:05 -0400

    added an awesome comment

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -6,6 +6,7 @@ from flask_login import current_user, login_user, logout_user, login_required
 
 auth_routes = Blueprint('auth', __name__)
 
+# adding a commit to change the files
 
 def validation_errors_to_error_messages(validation_errors):
     """
